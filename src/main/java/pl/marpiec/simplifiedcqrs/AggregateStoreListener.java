8c5d02524df302baec560f3d0eb3f20410d9dc21
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2013-03-20 15:14:18 +0100

    Refactorization to use interface instead of abstract class

--- src/main/java/pl/marpiec/simplifiedcqrs/AggregateStoreListener.java
+++ src/main/java/pl/marpiec/simplifiedcqrs/AggregateStoreListener.java
@@ -3,11 +3,7 @@ package pl.marpiec.simplifiedcqrs;
 /**
  *
  */
-public abstract class AggregateStoreListener<T extends Aggregate> {
+public interface AggregateStoreListener<T extends Aggregate> {
 
-    public final void startListeningToAggregateStore(AggregateStore aggregateStore, Class<T> aggregateClass) {
-        aggregateStore.addListener(aggregateClass, this);
-    }
-
-    public abstract void onAggregateChanged(Aggregate aggregate);
+    void onAggregateChanged(Aggregate aggregate);
 }
